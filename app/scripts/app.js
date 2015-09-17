dff0ca072e7db97fae88ea717ea66711c45f987a
Lucas Soler <solerl@estei.fr>
2015-09-17 13:17:28 +0200

    responsive navigation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,8 +5,8 @@ window.React = React;
 const mountNode = document.getElementById('app');
 
 function render () {
-  var route = window.location.hash.substr(2);
-  React.render(<Home route={route} />, mountNode);
+  	var route = window.location.hash.substr(2);
+  	React.render(<Home route={route} />, mountNode);
 }
 new WOW().init();
 
